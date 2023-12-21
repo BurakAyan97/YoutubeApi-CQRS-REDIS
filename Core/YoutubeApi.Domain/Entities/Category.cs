b988133cbb50d351e0e9f7989454806414b2faae
BurakAyan97 <burakayan97@outlook.com>
2023-12-22 00:42:25 +0300

    first entities created

---
+++ Core/YoutubeApi.Domain/Entities/Category.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Category : EntityBase
+    {
+        public Category()
+        {
+
+        }
+
+        //required alanları görebilmek için faydası var.(required property yeni bir instance oluşunca doldurulması zorunludur demek)
+        public Category(int parentId, string name, int priorty)
+        {
+            ParentId = parentId;
+            Name = name;
+            Priorty = priorty;
+        }
+
+        public required int ParentId { get; set; }
+        public required string Name { get; set; }
+        public required int Priorty { get; set; }
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
